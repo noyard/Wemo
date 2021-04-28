382275c32aa9d696171652b6b12b805d6ca9a5c4
noyard <noyard@microsoft.com>
2021-04-28 17:13:47 -0500

    4/27 update

--- Wemo.ps1
+++ Wemo.ps1
@@ -10,38 +10,11 @@ function Get-IpPort
         [String]$Port
     )
 
-    $ErrorActionPreference = "SilentlyContinue"
-
-    # Create TCP Client
-    $tcpclient = new-Object system.Net.Sockets.TcpClient
-    
-    # Tell TCP Client to connect to machine on Port
-    $iar = $tcpclient.BeginConnect($ipAddress,$port,$null,$null)
-
-    # Set the wait time
-    $wait = $iar.AsyncWaitHandle.WaitOne($timeout,$false)
-
-    # Check to see if the connection is done
-    if(!$wait)
-    {
-        # Close the connection and report timeout
-        $tcpclient.Close()
-        $ErrorActionPreference = "Continue"
-        if($verbose){Write-Host "Connection Timeout"}
-        Return $false
+    if(test-netconnection $ipAddress -port $port -InformationLevel Quiet -WarningAction SilentlyContinue)
+        {return $true}
+    else {
+        return $false
     }
-    else
-    {
-        # Close the connection and report the error if there is one
-        $error.Clear()
-        $tcpclient.EndConnect($iar) | out-Null
-        if(!$?){if($verbose){write-host $error[0]};$failed = $true}
-        $tcpclient.Close()
-        $ErrorActionPreference = "Continue"
-    }
-
-    # Return $true if connection Establish else $False
-    if($failed){return $false}else{return $true}
 }
 
 function Get-Wemo
@@ -52,45 +25,31 @@ function Get-Wemo
         [String[]]$ipAddress
     )
     #depending on device and firmware, a WeMo can listen on a number of ports
-    $ports = (49152,49153,49154,49155)
+    $ports = 49152,49153,49154,49155
 
     $Wemoport=$null
     foreach ($port in $ports)
     {
-        if(Get-IpPort $ipAddress $port) {
+        if(Get-IpPort $($ipAddress) $($port)) {
             "Wemo is online at $($port)"
             $Wemoport = $port
         }
     }
-
-    if ($null -ne $Wemoport){
-        $url = "http://$($ipAddress):$($port)/upnp/control/basicevent1"
-        $body = @"
-    <?xml version="1.0" encoding="utf-8"?>
-    <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-        <s:Body>
-            <u:SetBinaryState xmlns:u="urn:Belkin:service:basicevent:1">
-                <BinaryState>1</BinaryState>
-            </u:SetBinaryState>
-        </s:Body>
-    </s:Envelope>
-    "@
-
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-        $result = Invoke-WebRequest -Uri $url -Body $body -ContentType "text/xml" -Method POST -Headers @{"SOAPACTION" = "`"urn:Belkin:service:basicevent:1#SetBinaryState`""}
-    }else{
-        "Wemo offline"
-    }
+    #return $Wemoport
 }
+
 
 function Set-WemoOff
 {
     param (
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String[]]$ipAddress
+        [String]$ipAddress,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Port
     )
-    $port = "49153"
     $url = "http://$($ipAddress):$($port)/upnp/control/basicevent1"
     $body = @"
 <?xml version="1.0" encoding="utf-8"?>
@@ -103,17 +62,16 @@ function Set-WemoOff
 </s:Envelope>
 "@
 
-        $connection = New-Object System.Net.Sockets.TcpClient($ipaddress, $port)
-        if ($connection.Connected) {
-            Write-Host "Wemo is online"
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-            $result = Invoke-WebRequest -Uri $url -Body $body -ContentType "text/xml" -Method POST -Headers @{"SOAPACTION" = "`"urn:Belkin:service:basicevent:1#SetBinaryState`""}
-            return $result
-        } else { 
-            Write-Host "Failed to connect to Wemo" 
-            return 404
-        }
-    }
+$connection = New-Object System.Net.Sockets.TcpClient($ipaddress, $port)
+if ($connection.Connected) {
+    Write-Host "Wemo is online"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+    $result = Invoke-WebRequest -Uri $url -Body $body -ContentType "text/xml" -Method POST -Headers @{"SOAPACTION" = "`"urn:Belkin:service:basicevent:1#SetBinaryState`""}
+    return $result
+} else { 
+    Write-Host "Failed to connect to Wemo" 
+    return 404
+}
 }
 
 function Set-WemoOn
@@ -121,9 +79,12 @@ function Set-WemoOn
     param (
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [String[]]$ipAddress
+        [String]$ipAddress,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Port
     )
-    $port = "49153"
     $url = "http://$($ipAddress):$($port)/upnp/control/basicevent1"
     $body = @"
 <?xml version="1.0" encoding="utf-8"?>
